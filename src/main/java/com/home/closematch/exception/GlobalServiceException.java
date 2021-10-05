99a790856b324d9da942c48b589dd893ae848f2a
zhiyuan <71120450@qq.com>
2021-10-06 01:28:36 +0800

    全局异常处理

--- src/main/java/com/home/closematch/exception/GlobalServiceException.java
+++ src/main/java/com/home/closematch/exception/GlobalServiceException.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+/**
+ * 异常处理类
+ */
 @Slf4j
 @RestControllerAdvice
 public class GlobalServiceException {
